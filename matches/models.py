15960f5f01b7bdc9527d64b009e778c3d8a835c0
Lukasz <lukasz.luxxxx@gmail.com>
2023-04-26 14:38:35 +0200

    related names

--- matches/models.py
+++ matches/models.py
@@ -4,6 +4,7 @@ from teams.models import Team, Player
 
 EVENT_TYPES = (
     ('G', 'Goal'),
+    ('A', 'Assist'),
     ('Y', 'YellowCard'),
     ('R', 'RedCard'),
     ('S', 'Substitution'),
@@ -12,12 +13,12 @@ EVENT_TYPES = (
 
 
 class Match(models.Model):
-    date = models.DateTimeField()
+    date = models.DateField()
     stage = models.CharField(max_length=20)
-    home_team = models.ForeignKey(Team, on_delete=models.CASCADE)
-    away_team = models.ForeignKey(Team, on_delete=models.CASCADE)
-    home_goals = models.IntegerField(min_value=0, max_value=20)
-    away_goals = models.IntegerField(min_value=0, max_value=20)
+    home_team = models.ForeignKey(Team, on_delete=models.CASCADE, related_name='Home')
+    away_team = models.ForeignKey(Team, on_delete=models.CASCADE, related_name='Away')
+    home_goals = models.IntegerField()
+    away_goals = models.IntegerField()
     events = models.ManyToManyField('Event')
 
     def __str__(self):
@@ -26,6 +27,6 @@ class Match(models.Model):
 
 class Event(models.Model):
     type = models.CharField(choices=EVENT_TYPES, max_length=2)
-    player1 = models.ForeignKey(Player, on_delete=models.CASCADE)
-    player2 = models.ForeignKey(Player, on_delete=models.CASCADE, blank=True, null=True)
+    player1 = models.ForeignKey(Player, on_delete=models.CASCADE, related_name='player1')
+    player2 = models.ForeignKey(Player, on_delete=models.CASCADE, blank=True, null=True, related_name='player2')
     clock = models.CharField(max_length=10)
